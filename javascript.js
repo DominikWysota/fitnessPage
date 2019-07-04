b1b5bd4faedb8bb7052ec7958bf2bf2e261d9e20
Dominik Wysota <Medutenza@gmail.com>
2019-07-04 15:49:58 +0200

    Add function to js on facilities div

--- javascript.js
+++ javascript.js
@@ -1,5 +1,30 @@
+const block = ['.flexFacilities>div:nth-child(1)', '.flexFacilities>div:nth-child(2)',
+    '.flexFacilities>div:nth-child(3)', '.flexFacilities>div:nth-child(4)'
+];
+
 $(document).ready(function () {
-    $('.flexFacilities>div:nth-child(1)').click(function () {
+    $(block[0]).click(function () {
         $(this).toggleClass("active");
+        for (let i = 1; i < 4; i++) {
+            $(block[i]).removeClass("active");
+        }
+    });
+    $(block[1]).click(function () {
+        $(this).toggleClass("active");
+        $(block[0]).removeClass("active");
+        $(block[2]).removeClass("active");
+        $(block[3]).removeClass("active");
+    });
+    $(block[2]).click(function () {
+        $(this).toggleClass("active");
+        $(block[0]).removeClass("active");
+        $(block[1]).removeClass("active");
+        $(block[3]).removeClass("active");
+    });
+    $(block[3]).click(function () {
+        $(this).toggleClass("active");
+        for (let i = 0; i < 3; i++) {
+            $(block[i]).removeClass("active");
+        }
     });
 })
