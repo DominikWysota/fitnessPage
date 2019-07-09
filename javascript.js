56a7583d1212464959d4a336cb3992ab4531d553
Dominik Wysota <Medutenza@gmail.com>
2019-07-09 16:58:21 +0200

    Add Gallery block

--- javascript.js
+++ javascript.js
@@ -2,7 +2,10 @@ const block = ['.flexFacilities>div:nth-child(1)', '.flexFacilities>div:nth-chil
     '.flexFacilities>div:nth-child(3)', '.flexFacilities>div:nth-child(4)'
 ];
 
+let photo = 0;
+
 $(document).ready(function () {
+    //Code to Facilities block
     $(block[0]).click(function () {
         $(this).toggleClass("active");
         for (let i = 1; i < 4; i++) {
@@ -25,6 +28,23 @@ $(document).ready(function () {
         $(this).toggleClass("active");
         for (let i = 0; i < 3; i++) {
             $(block[i]).removeClass("active");
+        }
+    });
+    //Code to Gallery block
+    $(".rightButton").click(function () {
+        if (photo < 3) {
+            photo++
+            $(".photo").animate({
+                left: '-=100%'
+            });
+        }
+    });
+    $(".leftButton").click(function () {
+        if (photo > 0) {
+            photo--
+            $(".photo").animate({
+                left: '+=100%'
+            });
         }
     });
 })
